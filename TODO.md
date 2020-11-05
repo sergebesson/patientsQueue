b089de98a5977c6556ed6a1de628bd46434e2236
Serge BESSON <serge.besson@sfr.com>
2020-11-05 17:15:21 +0100

    Création du module api `reasonRequest`

--- TODO.md
+++ TODO.md
@@ -2,15 +2,14 @@
 
 ## TODO
 
-* Modification données de patientQueue :
-  * patientQueue.phoneNumbers : n'est plus une list mais juste un numéro, renommé en phoneNumber
-  * patientQueue.medicalInformation : Codification du motif de la demande (renommé en reasonRequest) et ajout d'un détail du motif de la demande
-  * patientQueue.contacts : Ajout d'un type de contact (sms, email, phone, face-to-face)
 * iu - filtre sur les différents state
 
 ## ON GOING
 
-* Ajout de l'API de gestion des `reasonRequest`
+* Modification données de patientQueue :
+  * patientQueue.phoneNumbers : n'est plus une list mais juste un numéro, renommé en phoneNumber
+  * patientQueue.medicalInformation : Codification du motif de la demande (renommé en reasonRequest) et ajout d'un détail du motif de la demande
+  * patientQueue.contacts : Ajout d'un type de contact (sms, email, phone, face-to-face)
 
 ## DONE
 
@@ -19,6 +18,7 @@
 * Création du module api `patientsQueue`
 * iu - liste des patientQueue en cours
 * Ajout d'un nouveau store `reasonRequest` avec juste id et label
+* Ajout de l'API de gestion des `reasonRequest`
 
 ## Info
 
---
+++ server/routers/api/modules/reason-request/reason-request.module.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const express = require("express");
+const { camelCaseDeep } = require("../../services/change-case.service");
+
+const authorizedRoutes = {
+	admin: [ "/?", "/[^/]+" ],
+};
+
+function routerFactory({ stores: { reasonRequestStore } }) {
+	const router = express.Router(); // eslint-disable-line new-cap
+
+	router.get("/", (request, response) => {
+		response.status(200).json(camelCaseDeep(reasonRequestStore.search({})));
+	});
+
+	router.get("/:id", (request, response) => {
+		const reasonRequest = reasonRequestStore.get(request.params.id);
+		if (!reasonRequest) {
+			return response.status(404).json({
+				status: 404,
+				error_description: "Not Found",
+			});
+		}
+		response.status(200).json(camelCaseDeep(reasonRequest));
+	});
+
+	router.post("/", (request, response, next) => reasonRequestStore.insert(request.body.label)
+		.then((reasonRequest) => response.status(201).json(camelCaseDeep(reasonRequest)))
+		.catch(next),
+	);
+
+	router.patch("/:id", (request, response, next) => reasonRequestStore.update(
+		request.params.id, request.body.label,
+	)
+		.then((reasonRequest) => response.status(200).json(camelCaseDeep(reasonRequest)))
+		.catch((error) => error.message === "notExist" ?
+			response.status(404).json({
+				status: 404,
+				error_description: "Not Found",
+			}) :
+			next(error),
+		),
+	);
+
+	router.delete("/:id", (request, response, next) => reasonRequestStore.delete(request.params.id)
+		.then(() => response.sendStatus(204))
+		.catch((error) => error.message === "notExist" ?
+			response.status(404).json({
+				status: 404,
+				error_description: "Not Found",
+			}) :
+			next(error),
+		),
+	);
+
+	return router;
+}
+
+module.exports = {
+	authorizedRoutes,
+	routerFactory,
+};
